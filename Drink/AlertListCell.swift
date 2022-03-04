420ca1f67b3b1168f086431f62ffad9948628867
youngmo <youngmo0806@naver.com>
2022-03-04 14:07:35 +0900

    first Commit

---
+++ Drink/AlertListCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlertListCell.swift
+//  Drink
+//
+//  Created by youngmo on 2022/03/04.
+//
+
+import UIKit
+
+class AlertListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
